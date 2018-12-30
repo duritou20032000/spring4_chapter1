024293ae56b12ae133a64ea911921a11cd80aa50
Administrator <duritou20032000@163.com>
2018-12-30 13:00:29 +0800

    Spring4_learn_chapter1

---
+++ src/main/java/com/example/spring/App.java
@@ -0,0 +1,20 @@
+package com.example.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        /**
+         * hello
+         */
+        System.out.println( "Hello World!");
+
+    }
+}
---
+++ src/test/java/com/example/spring/AppTest.java
@@ -0,0 +1,31 @@
+package com.example.spring;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Unit test for simple App.
+ */
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void HelloTest(){
+
+    }
+}
